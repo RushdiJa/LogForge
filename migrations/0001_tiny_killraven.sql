0bd3f7a833d25c8581070081038dea5051a43dd5
Rushdi Jallad <rushdi.jallad@gmail.com>
2026-08-11 17:42:45 +0300

    perf(db): tune PostgreSQL for write-heavy workload

--- migrations/0001_tiny_killraven.sql
+++ migrations/0001_tiny_killraven.sql
@@ -1,1 +1,8 @@
--- DROP INDEX "logs_level_idx";
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX "logs_message_trgm_idx"
+ON "logs"
+USING GIN ("message" gin_trgm_ops);
+
+CREATE INDEX IF NOT EXISTS "logs_message_trgm_idx"
+ON "logs"
+USING GIN ("message" gin_trgm_ops);
